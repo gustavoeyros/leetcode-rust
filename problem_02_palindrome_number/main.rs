972db12862e0558c4850c6b30f9fd378366f8298
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-10-31 13:25:46 -0300

    feat: palindrome number

---
+++ problem_02_palindrome_number/main.rs
@@ -0,0 +1,19 @@
+fn is_palindrome(x: i32) -> bool {
+    if x < 0 {
+        return false;
+    }
+    let mut num = x;
+    let mut rev = 0;
+
+    while num > 0 {
+        rev = rev * 10 + num % 10;
+        num /= 10;
+    }
+
+    rev == x
+}
+
+fn main() {
+    let result = is_palindrome(121);
+    println!("{:?}", result);
+}
